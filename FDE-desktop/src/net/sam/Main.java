69d7991ca1e24495c5f28c60e2432f8a196b6203
Skwix <Skwix@hotmail.co.uk>
2014-04-03 19:53:37 +0100

    k

--- FDE-desktop/src/net/sam/Main.java
+++ FDE-desktop/src/net/sam/Main.java
@@ -2,7 +2,7 @@ package net.sam;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-
+//
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
